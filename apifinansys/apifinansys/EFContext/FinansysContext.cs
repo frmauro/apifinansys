25b01e87205f530dc765d591616c9363c222ac1a
Francisco Mauro de Lima <francisco.mauro@fgv.br>
2019-03-28 16:38:24 -0300

    Create custom  class DbContext

---
+++ apifinansys/apifinansys/EFContext/FinansysContext.cs
@@ -0,0 +1,20 @@
+﻿using apifinansys.entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apifinansys.EFContext
+{
+    public class FinansysContext : DbContext
+    {
+        public FinansysContext(DbContextOptions<FinansysContext> options) 
+            :base(options)
+        {
+        }
+
+        public DbSet<Entry> Entries { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
