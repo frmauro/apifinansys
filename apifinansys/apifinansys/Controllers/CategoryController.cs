e44615d08a002a1a25ee77a21b1723f0d8cad3be
Francisco Mauro de Lima <francisco.mauro@fgv.br>
2019-03-29 11:02:18 -0300

    Create CategoryController api

---
+++ apifinansys/apifinansys/Controllers/CategoryController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using apifinansys.EFContext;
+using apifinansys.entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace apifinansys.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly FinansysContext _context;
+
+        public CategoryController(FinansysContext context)
+        {
+            this._context = context;
+        }
+
+        // GET: api/Category
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Category>>> GetCategories()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+
+
+        // GET: api/Category/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Category>> GetCategory(long id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+
+            if (category == null)
+                return NotFound();
+
+            return category;
+        }
+    }
+}
